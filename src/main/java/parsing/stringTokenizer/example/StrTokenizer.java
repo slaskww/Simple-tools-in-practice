c7a53c24c594c259b3691cb87ce1f64f05b17890
lukasz <slaskww@gmail.com>
2019-11-05 09:16:00 +0100

    Add StringTokenizer example

---
+++ src/main/java/parsing/stringTokenizer/example/StrTokenizer.java
@@ -0,0 +1,37 @@
+package parsing.stringTokenizer.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+
+/**
+ * StringTokenizer is a legacy class that is retained for compatibility reasons ALTHOUGH ITS USE IS DISCOURAGE IN NEW CODE.
+ * It is RECOMMENDED that anyone seeking this functionality use the SPLIT METHOD OF SPRING or the JAVA.UTIL.REGEX package instead.
+ */
+
+public class StrTokenizer {
+
+    public static List<String> tokenizuj(String source, String separator){
+        List<String> result = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(source, separator);
+
+        System.out.println("method countTokens(): " + st.countTokens());
+
+        while (st.hasMoreTokens()){
+            result.add(st.nextToken());
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        String source = "The   string tokenizer\nclass allows.an application+to.break a string into tokens.";
+        String separator = "' '\n+."; //separatorami są znak spacji, nowej linii, '+' oraz '.'
+
+        List<String> res = tokenizuj(source, separator);
+        String resultWithJoin = String.join("-", res);
+        System.out.println(resultWithJoin);
+    }
+}
