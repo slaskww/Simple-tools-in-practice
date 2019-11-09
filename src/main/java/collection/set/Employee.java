f98f88d3096535a6fdc35a508e539c712f71c4eb
lukasz <slaskww@gmail.com>
2019-11-09 09:58:48 +0100

    Added HashSetTest

---
+++ src/main/java/collection/set/Employee.java
@@ -0,0 +1,67 @@
+package collection.set;
+
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Employee(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+
+        if (age != employee.age) return false;
+        if (firstName != null ? !firstName.equals(employee.firstName) : employee.firstName != null) return false;
+        return lastName != null ? lastName.equals(employee.lastName) : employee.lastName == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName != null ? firstName.hashCode() : 0;
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + age;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                "}\n";
+    }
+}
---
+++ src/main/java/collection/set/HashSetTests.java
@@ -0,0 +1,50 @@
+package collection.set;
+
+import org.junit.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+public class HashSetTests {
+
+    @Test
+    public void shouldSetContainsTheSameElements(){
+
+        /**
+         * Kolekcja będąca zbiorem powinna zawierać unikalne elementy. Unikalność ta zostaje weryfikowana metodami hashcode() i equals() definiowanych w klasie elementu.
+         * Jeśli set zawiera elementy modyfikowalne, to przy ich modyfikacji możemy doprowadzić do niespójności setu, co pokazano poniżej.
+         * W teście tworzymy obiekt setu i wypełniamy go referencjami do obiektów Employee. Każdy z elementów różni unikalne nazwisko.
+         * Kiedy zmodyfikujemy pole z wartością nazwiska w taki sposób, aby kazdy z elementów otrzymał tę samą wartość lastName, to każdy z elementów setu
+         * wygeneruje identyczny hashcode.
+         * Okaże się więc, że set przechowuje trzy identyczne elementy, co przeczy definicji zbioru.
+         */
+
+        HashSet<Employee> set = new HashSet<>(Arrays.asList(
+                new Employee("Adam", "Niezgódka", 40),
+                new Employee("Adam", "Malysz", 40),
+                new Employee("Adam", "Wojcik", 40)
+        ));
+
+        System.out.println(set);
+        assertEquals(3, set.size());
+
+
+        for (Employee e :set){
+            e.setLastName("Doe");
+        }
+
+        assertEquals(3, set.size());
+
+        List<Integer> hashcodes = new ArrayList<>();
+        for (Employee e : set){
+            hashcodes.add(e.hashCode());
+        }
+
+        assertEquals(hashcodes.get(0), hashcodes.get(1));
+        assertEquals(hashcodes.get(0), hashcodes.get(2));
+
+        }
+
+}
