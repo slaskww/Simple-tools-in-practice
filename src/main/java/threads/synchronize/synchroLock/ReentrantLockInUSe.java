a05cbba65e6e646529b2bdee6e772a7e07dd1a12
lukasz <slaskww@gmail.com>
2019-12-10 15:35:33 +0100

    Add description

--- src/main/java/threads/synchronize/synchroLock/ReentrantLockInUSe.java
+++ src/main/java/threads/synchronize/synchroLock/ReentrantLockInUSe.java
@@ -4,9 +4,26 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * Obiekty klasy ReentrantLock odpowiadają mechanizmowi synchronizacji za pomocą słowa synchronized.
+ * W odróżnieniu do synchronizacji z synchronized obiekty ReentrantLock:
+ *      -mogą być przekazywane przez referencję jako argumenty metod lub w konstruktorach
+ *       a co za tym idzie, moga być zamykane (lock) i otwierane (unlock) w różnych sekcjach kodu wykonywanego przez dany wątek
+ *       (synchronizacja ze słowem synchronized moze być uzyte tylko w ramach tego samego bloku)
+ *      -umożliwiaja sprawdzenie, czy rygiel jest zamknięty, co daje możliwość warunkowego zareagowania innego wątku na istnienie takiej blokady, tryLock()
+ *      -umożliwiaja sprawdzenie, czy rygiel jest zamknięty i ewentualne oczekiwanie na jego otwarcie przez określony czas, tryLock(...)
+ *      -umożliwiają przerwanie wątku, który jest zablokowany w oczekiwaniu na otwarcie rygla, lockInterruptibly()
+ *
+ *      Schemat działania obiektu rygla wygląda nastepująco:
+ *          1. Tworzymy obiekt rygla
+ *          2. Zamykamy rygiel wywołują metode lock()
+ *          3. Wykonywany jest kod sekcji krytycznej
+ *          4. Otwieramy rygiel metoda unlock()
+ */
+
 public class ReentrantLockInUSe {
 
-    private ReentrantLock lock = new ReentrantLock();
+    private ReentrantLock lock = new ReentrantLock(); //Tworzymy obiekt rygla
 
     private int number;
 
@@ -16,13 +33,19 @@ public class ReentrantLockInUSe {
 
     private void increase(String name) throws InterruptedException {
 
-        lock.lock();
-        System.out.println("Thread " + name + ": initial value = " + number);
-        number++;
-        Thread.sleep(1000);
-        System.out.println("Thread " + name + ": increased value = " + number);
-        lock.unlock();
 
+        lock.lock(); // zamykamy rygiel i dalej wykonujemy kod sekcji krytycznej
+
+        try{
+
+            System.out.println("Thread " + name + ": initial value = " + number);
+            number++;
+            Thread.sleep(1000);
+            System.out.println("Thread " + name + ": increased value = " + number);
+
+        } finally {
+            lock.unlock(); // otwieramy rygiel w bloku finally, by zapewnić zwolnienie blokady w każdych warunkach, również w przypadku wystąpienia wyjątku w sekcji krytycznej
+        }
     }
 
     public void executeThreads(int tnumber){
